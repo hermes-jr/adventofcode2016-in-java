a974d0a99bf02cb479e4423b1e94a640569d4138
Mikhail Antonov <hermes@compot.ru>
2016-12-17 18:34:25 +0300

    level_04 part 2

--- level_04/Level04.java
+++ level_04/Level04.java
@@ -8,17 +8,47 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
+ * --- Day 4: Security Through Obscurity ---
+ * <p>
+ * Finally, you come across an information kiosk with a list of rooms. Of course, the list is encrypted and full of decoy data, but the instructions to decode the list are barely hidden nearby. Better remove the decoy data first.
+ * <p>
+ * Each room consists of an encrypted name (lowercase letters separated by dashes) followed by a dash, a sector ID, and a checksum in square brackets.
+ * <p>
+ * A room is real (not a decoy) if the checksum is the five most common letters in the encrypted name, in order, with ties broken by alphabetization. For example:
+ * <p>
+ * aaaaa-bbb-z-y-x-123[abxyz] is a real room because the most common letters are a (5), b (3), and then a tie between x, y, and z, which are listed alphabetically.
+ * a-b-c-d-e-f-g-h-987[abcde] is a real room because although the letters are all tied (1 of each), the first five are listed alphabetically.
+ * not-a-real-room-404[oarel] is a real room.
+ * totally-real-room-200[decoy] is not.
+ * <p>
+ * Of the real rooms from the list above, the sum of their sector IDs is 1514.
+ * <p>
+ * What is the sum of the sector IDs of the real rooms?
+ * <p>
+ * <p>
+ * --- Part Two ---
+ * <p>
+ * With all the decoy data out of the way, it's time to decrypt this list and get moving.
+ * <p>
+ * The room names are encrypted by a state-of-the-art shift cipher, which is nearly unbreakable without the right software. However, the information kiosk designers at Easter Bunny HQ were not expecting to deal with a master cryptographer like yourself.
+ * <p>
+ * To decrypt a room name, rotate each letter forward through the alphabet a number of times equal to the room's sector ID. A becomes B, B becomes C, Z becomes A, and so on. Dashes become spaces.
+ * <p>
+ * For example, the real name for qzmt-zixmtkozy-ivhz-343 is very encrypted name.
+ * <p>
+ * What is the sector ID of the room where North Pole objects are stored?
  */
 public class Level04 {
 
 	public static void main(String[] args) throws IOException {
 		List<String> content = Files.readAllLines(Paths.get("level_04/in1.txt"));
 
-		Pattern abbas = Pattern.compile("^.*(.)((?!\\1).)\\2\\1.*$");
 		Pattern checksumPattern = Pattern.compile("\\[([a-z]+)\\]");
 		Pattern roomNumberPattern = Pattern.compile("([0-9]+)\\[[a-z]+\\]$");
 
 		int result1 = 0;
+
+		Map<String, Integer> cleanList = new HashMap<>();
 
 		lineLoop:
 		for (String line : content) {
@@ -58,13 +88,25 @@ public class Level04 {
 			}
 
 			System.out.println("YEEEHAAA");
+			cleanList.put(line.substring(0, line.indexOf("[")), roomNumber);
 			result1 += roomNumber;
 		}
 
 		System.out.println("result1 : " + result1);
+
+		// System.out.println("test: " + rotX("qzmt-zixmtkozy-ivhz", 343));
+
+		Integer result2 = 0;
+		for (Map.Entry<String, Integer> checkMe : cleanList.entrySet()) {
+			if (rotX(checkMe.getKey(), checkMe.getValue()).contains("north")) {
+				result2 = checkMe.getValue();
+				break;
+			}
+		}
+		System.out.println("result2 : " + result2);
 	}
 
-	public static class ValueThenKeyComparator<K extends Comparable<? super K>,
+	private static class ValueThenKeyComparator<K extends Comparable<? super K>,
 			V extends Comparable<? super V>>
 			implements Comparator<Map.Entry<K, V>> {
 
@@ -77,5 +119,22 @@ public class Level04 {
 			}
 		}
 
+	}
+
+	private static String rotX(String source, Integer rotations) {
+		StringBuilder result = new StringBuilder();
+		for (char rotMe : source.toCharArray()) {
+			for (int i = 0; i < rotations; i++) {
+				if (rotMe == '-' || rotMe == ' ') {
+					rotMe = ' ';
+					continue;
+				}
+				rotMe++;
+				if (rotMe == 'z' + 1)
+					rotMe = 'a';
+			}
+			result.append(rotMe);
+		}
+		return result.toString();
 	}
 }
