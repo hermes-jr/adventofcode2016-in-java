1fb4e37202895daa8100f8bf77fb451f21b34bec
Mikhail Antonov <hermes@compot.ru>
2016-12-25 10:09:14 +0300

    level 19 placeholder

---
+++ level_19/Level19.java
@@ -0,0 +1,37 @@
+package level_19;
+
+/**
+ * --- Day 19: An Elephant Named Joseph ---
+ * <p>
+ * The Elves contact you over a highly secure emergency channel. Back at the North Pole, the Elves are busy misunderstanding White Elephant parties.
+ * <p>
+ * Each Elf brings a present. They all sit in a circle, numbered starting with position 1. Then, starting with the first Elf, they take turns stealing all the presents from the Elf to their left. An Elf with no presents is removed from the circle and does not take turns.
+ * <p>
+ * For example, with five Elves (numbered 1 to 5):
+ * <p>
+ * 1
+ * 5   2
+ * 4 3
+ * <p>
+ * Elf 1 takes Elf 2's present.
+ * Elf 2 has no presents and is skipped.
+ * Elf 3 takes Elf 4's present.
+ * Elf 4 has no presents and is also skipped.
+ * Elf 5 takes Elf 1's two presents.
+ * Neither Elf 1 nor Elf 2 have any presents, so both are skipped.
+ * Elf 3 takes Elf 5's three presents.
+ * <p>
+ * So, with five Elves, the Elf that sits starting in position 3 gets all the presents.
+ * <p>
+ * With the number of Elves given in your puzzle input, which Elf gets all the presents?
+ * <p>
+ * Your puzzle input is 3005290.
+ */
+public class Level19 {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
