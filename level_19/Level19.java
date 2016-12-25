12268e407a0ad71ca52a7db0b36950c5f7f209b7
hermes-jr <hermes@cyllene.net>
2016-12-26 00:45:49 +0300

    level 19

--- level_19/Level19.java
+++ level_19/Level19.java
@@ -26,6 +26,49 @@ package level_19;
  * With the number of Elves given in your puzzle input, which Elf gets all the presents?
  * <p>
  * Your puzzle input is 3005290.
+
+--- Part Two ---
+
+Realizing the folly of their present-exchange rules, the Elves agree to instead steal presents from the Elf directly across the circle. If two Elves are across the circle, the one on the left (from the perspective of the stealer) is stolen from. The other rules remain unchanged: Elves with no presents are removed from the circle entirely, and the other elves move in slightly to keep the circle evenly spaced.
+
+For example, with five Elves (again numbered 1 to 5):
+
+    The Elves sit in a circle; Elf 1 goes first:
+
+      1
+    5   2
+     4 3
+
+    Elves 3 and 4 are across the circle; Elf 3's present is stolen, being the one to the left. Elf 3 leaves the circle, and the rest of the Elves move in:
+
+      1           1
+    5   2  -->  5   2
+     4 -          4
+
+    Elf 2 steals from the Elf directly across the circle, Elf 5:
+
+      1         1 
+    -   2  -->     2
+      4         4 
+
+    Next is Elf 4 who, choosing between Elves 1 and 2, steals from Elf 1:
+
+     -          2  
+        2  -->
+     4          4
+
+    Finally, Elf 2 steals from Elf 4:
+
+     2
+        -->  2  
+     -
+
+So, with five Elves, the Elf that sits starting in position 2 gets all the presents.
+
+With the number of Elves given in your puzzle input, which Elf now gets all the presents?
+
+Your puzzle input is still 3005290.
+
  */
 public class Level19 {
 
@@ -42,6 +85,15 @@ public class Level19 {
 		int result1 = (target - maxpow) * 2 + 1;
 		System.out.println("result1: " + result1);
 
+		int result2 = 1;
+		for (int i = 1; i < target; i++) {
+			result2 %= i;
+			result2++;
+			if ((i + 1) / 2 < result2) {
+				result2++;
+			}
+		}
+		System.out.println("result2: " + result2);
 	}
 
 }
